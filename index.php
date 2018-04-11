a1f5721208c8382f7bde39732e392eedecec92e3
joni.sutinen95@gmail.com <joni.sutinen95@gmail.com>
2018-04-11 15:27:41 +0300

    lisätty lopputagit

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 include('head.php');
 ?>
 <div class="container">
+  <?php include('movievisualizer.php'); ?>
+  <?php include('personvisualizer.php'); ?>
+
   <table class="table table-striped table-hover table-bordered table-responsive-sm">
     <thead class="thead-dark">
       <tr>
@@ -59,6 +62,8 @@ include('head.php');
   </table>
 </div>
 
-<?php
-  include('footer.php');
-?>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>

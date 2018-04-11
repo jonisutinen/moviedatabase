21eb4344175f8b44c67910998cc6cb4c9ee6b4c8
joni.sutinen95@gmail.com <joni.sutinen95@gmail.com>
2018-04-11 12:38:56 +0300

    sivujen alut ja loput samoista tiedostoista jotta navbaria ja footereita helpompi muokata

---
+++ head.php
@@ -0,0 +1,31 @@
+<?php
+/*Just for your server-side code*/
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Elokuvatietokanta</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">MovieDB</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="index.php">Kaikki elokuvat<span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="lisaauusi.php">Lisää uusi</a>
+        </div>
+      </div>
+    </div>
+  </nav>

b757a5b755d7fd6fa6251f53daee13724d2e1277
joni.sutinen95@gmail.com <joni.sutinen95@gmail.com>
2018-04-11 17:24:23 +0300

    lisätty visualisoimaan sisältöä

---
+++ countryvisualizer.php
@@ -0,0 +1,16 @@
+<?php
+include('includes/dbconn.inc.php');
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql1 = "SELECT * FROM Valmistusmaa";
+
+if($result = mysqli_query($conn, $sql1)){
+  $rowcount = mysqli_num_rows($result);
+  echo $rowcount;
+} else{
+    echo "1ERROR: Could not able to execute $sql1. " . mysqli_error($conn);
+}
+
+?>
